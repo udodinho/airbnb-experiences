34941c3247aee86da1f4b6a9fe03e978dcf4c959
udodinho <udodinho200@gmail.com>
2024-01-15 18:24:24 +0100

    Add data

---
+++ src/data.js
@@ -0,0 +1,192 @@
+import Kate from "./img/zafares.png"
+import Photography from "./img/wedding-photography.png"
+import Bike from "./img/mountain-bike.png"
+import York from "./img/york.jpeg"
+import Barca from "./img/barca.jpeg"
+import Tokyo from "./img/tokyo.jpeg"
+import Nashville from "./img/nashville.jpeg"
+import Magic from "./img/magic.jpeg"
+import Kyoto from "./img/kyoto.jpeg"
+import Japan from "./img/japan.jpeg"
+import Tulum from "./img/tulum.jpeg"
+import Wild from "./img/wild-life.jpeg"
+import Vacation from "./img/vacation.jpeg"
+
+
+ const data = [
+    {
+        id: 1,
+        title: "Life lessons with katie zafares",
+        description: "",
+        price: 136,
+        coverImg: Kate,
+        stats: {
+            rating: 5.0,
+            reviewCount: 6,
+        },
+        location: "USA",
+        openSpots: 0,
+    },
+    {
+        id: 2,
+        title: "Learn Wedding Photography",
+        description: "Interested in becoming a wedding photographer? For beginner and experienced photographers alike, join us in learnng techniques required to leave the happy couples  memories that i'll last a lifetime.",
+        price: 125,
+        coverImg: Photography,
+        stats: {
+            rating: 5.0,
+            reviewCount: 30,
+        },
+        location: "Online",
+        openSpots: 27,
+    },
+    {
+        id: 3,
+        title: "Group mountain Biking",
+        description: "Experience the beautiful Norwegian landscape and meet new friends all while conquering rugged terrain on your mountain bike. (Bike provided!)",
+        price: 50,
+        coverImg: Bike,
+        stats: {
+            rating: 4.8,
+            reviewCount: 2,
+        },
+        location: "Norway",
+        openSpots: 3,
+    },
+    {
+        id: 4,
+        title: "Plan The Perfect New York Vacation",
+        description: "",
+        price: 19,
+        coverImg: York,
+        stats: {
+            rating: 4.99,
+            reviewCount: 168,
+        },
+        location: "United States",
+        openSpots: 7,
+    },
+    {
+        id: 5,
+        title: "Live Tulum like a local",
+        description: "",
+        price: 21,
+        coverImg: Tulum,
+        stats: {
+            rating: 4.71,
+            reviewCount: 14,
+        },
+        location: "Mexico",
+        openSpots: 18,
+    },
+    {
+        id: 6,
+        title: "Plan A Trip To Nashville With a Local Insider",
+        description: "",
+        price: 36,
+        coverImg: Nashville,
+        stats: {
+            rating: 4.98,
+            reviewCount: 43,
+        },
+        location: "Nashville",
+        openSpots: 12,
+    },
+    {
+        id: 7,
+        title: "Live Virtual Wildlife Safari in South Africa",
+        description: "",
+        price: 21,
+        coverImg: Wild,
+        stats: {
+            rating:  4.93,
+            reviewCount: 736,
+        },
+        location: "South Africa",
+        openSpots: 4,
+    },
+    {
+        id: 8,
+        title: "Secrets of Magic",
+        description: "",
+        price: 25,
+        coverImg: Magic,
+        stats: {
+            rating: 4.98,
+            reviewCount: 16,
+        },
+        location: "United Kingdom",
+        openSpots: 6,
+    },
+    {
+        id: 9,
+        title: "Learn & Plan a Kyoto trip with local advisor Yuko",
+        description: "",
+        price: 8,
+        coverImg: Kyoto,
+        stats: {
+            rating: 5.0,
+            reviewCount: 11,
+        },
+        location: "Japan",
+        openSpots: 5,
+    },
+    {
+        id: 10,
+        title: "Plan The Perfect Barcelona Vacation",
+        description: "",
+        price: 17,
+        coverImg: Barca,
+        stats: {
+            rating: 5.0,
+            reviewCount: 16,
+        },
+        location: "Spain",
+        openSpots: 31,
+
+    },
+    {
+        id: 11,
+        title: "Plan A Dream Japan Vacation",
+        description: "",
+        price: 18,
+        coverImg: Japan,
+        stats: {
+            rating: 5.0,
+            reviewCount: 5,
+        },
+        location: "Japan",
+        openSpots: 21,
+    },
+    {
+        id: 11,
+        title: "Plan the perfect Customised Japan Trip with Local Expert",
+        description: "",
+        price: 45,
+        coverImg: Vacation,
+        stats: {
+            rating: 5.0,
+            reviewCount: 37,
+        },
+        location: "Japan",
+        openSpots: 10,
+    },
+    {
+        id: 11,
+        title: "Design a perfect Tokyo trip with a Tokyo expert",
+        description: "",
+        price: 8,
+        coverImg: Tokyo,
+        stats: {
+            rating: 5.0,
+            reviewCount: 2,
+        },
+        location: "Japan",
+        openSpots: 1,
+    },
+]
+
+
+
+
+export default data;
